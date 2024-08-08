2bdaa2ca6e7d71587d36e8b44dd93260e0564baf
elgfe <170134446+slothheadmuffin@users.noreply.github.com>
2024-08-07 18:47:28 -0700

    Added functionality to buttons

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const timer=document.querySelector("#timer");
 const minutoSpan=document.querySelector("#minutos");
 const segundoSpan=document.querySelector("#segundos");
-const botonesTimers=document.querySelectorAll(".timers");
+const play=document.querySelector("#play");
+const botonesTimers=document.querySelector("#botonesTimers");
 let segundos=segundoSpan.textContent;
 let minutos=minutoSpan.textContent;
+
+botonesTimers.addEventListener("click",(e)=>{
+    switch (e.target.id){
+        case 'work':
+            minutoSpan.textContent='25';
+            break;
+        case 'descansoCorto':
+            minutoSpan.textContent='05';
+            break;
+        case 'descansoLargo':
+            minutoSpan.textContent='15';
+            break;
+    }
+})
+
+play.addEventListener("click",(e)=>{
+    setInterval(timerWork, 1000); 
+})
+
 function timerWork() {
     
     if (segundos==0){
@@ -18,5 +38,4 @@ function timerWork() {
     const segundosRestantes = segundos % 60;
     timer.textContent = `${minutos.toString().padStart(2, "0")}:${segundosRestantes.toString().padStart(2, "0")}`;
   }
-  
-  setInterval(timerWork, 1000); 
+ 
